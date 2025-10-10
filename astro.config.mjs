bef12d51a8f68f69217001cd92387a6fc5432caf
Robinxo <omrawat.work@gmail.com>
2025-10-10 17:59:15 +0530

    SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), sitemap()],
   server: {
     host: true, // allow external access
     strictPort: false,
@@ -13,4 +14,3 @@ export default defineConfig({
     ],
   },
 });
-
